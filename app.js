b99282d4b21688377b7344d3c8d4790f6e384a2a
Corey <simon8888xsimon8888x@gmail.com>
2020-09-09 15:29:48 +0800

    feat: Add userController and signup page

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const db = require('./models') 
+const bodyParser = require('body-parser')
 const PORT = 3000
 
 
 app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs"}))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routes')(app)
 
