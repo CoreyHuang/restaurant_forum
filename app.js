b57f6e5264e18fc4987a5e20846164371629df67
Corey <simon8888xsimon8888x@gmail.com>
2020-09-09 16:02:06 +0800

    feat: Confirm user password and unique email

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const exphbs = require('express-handlebars')
 const app = express()
 const db = require('./models') 
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const PORT = 3000
 
 
 app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs"}))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 require('./routes')(app)
 
