6f25cfb24f1a55aec70732463c333ea023457db6
Corey <simon8888xsimon8888x@gmail.com>
2020-09-15 16:31:48 +0800

    eat: Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,16 +5,28 @@ const Category = db.Category
 let restController = {
   getRestaurants: (req, res) => {
     Restaurant.findAll({ include: Category }).then(restaurants => {
-      console.log('restaurants', restaurants[0])
-      console.log('restaurants', restaurants[0].dataValues.description.substring(0, 50))
+      console.log('restaurants', restaurants[0].Category.dataValues.name)
+      console.log('restaurants', restaurants[0].Category.name)
+      // console.log('restaurants', restaurants[0].dataValues.description.substring(0, 50))
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
       }))
-      console.log('data', data[0])
+      // console.log('data', data[0])
       return res.render('restaurants', {
         restaurants: data
+      })
+    })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      // console.log('restaurant.toJSON()', restaurant.toJSON())
+      return res.render('restaurant', {
+        restaurant: restaurant.toJSON()
       })
     })
   }
