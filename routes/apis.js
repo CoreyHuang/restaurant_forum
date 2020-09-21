6e40e62f4e6d28f0815f5a603aabc52c441e78be
Corey <simon8888xsimon8888x@gmail.com>
2020-09-22 05:46:27 +0800

    refactor: adminController.deleteRestaurant

--- routes/apis.js
+++ routes/apis.js
@@ -8,5 +8,7 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', adminController.getCategories)
 
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
 
 module.exports = router
