d16739c33b558986f07b1e578f7eca449caa1a44
Corey <simon8888xsimon8888x@gmail.com>
2020-09-22 17:49:31 +0800

    feat: add PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -10,8 +10,10 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', adminController.getCategories)
 
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
-router.post('/admin/restaurants',upload.single('image'), adminController.postRestaurant)
+
 
 
 module.exports = router
